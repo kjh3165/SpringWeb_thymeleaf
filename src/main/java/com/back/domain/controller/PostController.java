0140ce68f86475529e353a78a1cf0059a6f7c073
kjh3165 <jhwk109@gmail.com>
2026-05-08 12:50:49 +0900

    글 작성 폼

--- src/main/java/com/back/domain/controller/PostController.java
+++ src/main/java/com/back/domain/controller/PostController.java
@@ -3,9 +3,25 @@ package com.back.domain.controller;
 import com.back.domain.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
 public class PostController {
     private final PostService postService;
+
+    @GetMapping("/posts/write")
+    @ResponseBody
+    public String write() {
+        return """
+                <form action="http://localhost:8080/posts/doWrite" target="_blank">
+                  <input type="text" name="title" placeholder="제목" value="안녕">
+                  <br>
+                  <textarea name="content" placeholder="내용">반가워</textarea>
+                  <br>
+                  <input type="submit" value="작성">
+                </form>
+                """;
+    }
 }
