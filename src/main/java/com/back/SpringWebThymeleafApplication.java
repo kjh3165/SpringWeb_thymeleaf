68e081ebc797e7dd8373651d0a54b60208a6c2b1
kjh3165 <jhwk109@gmail.com>
2026-05-08 12:13:14 +0900

    세팅

---
+++ src/main/java/com/back/SpringWebThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebThymeleafApplication.class, args);
+    }
+
+}
